d9729147bde7b8aa65e33887640b0fad72d39c34
Abhishek <abhishek9997107479@gmail.com>
2023-07-09 15:50:00 +0530

    first commit

---
+++ css/tut53.js
@@ -0,0 +1,25 @@
+console.log("This is tutorial53");
+function greet(name, greetText){
+    let name1 ="Name1";
+    console.log(greetText + " "+name);
+    console.log(name + " is a good boy");
+}
+function sum(a,b,c){
+    let d=a+b+c;
+    return d;
+}
+let name = "Abhishek";
+let name1= "Radhey";
+let name2 = "shyam";
+let name3 = "Rohan";
+let greetText = "Good morning";
+greet(name , greetText);
+greet(name1 , greetText);
+greet(name2 , greetText);
+greet(name3 , greetText);
+let returnval=sum(1,2,3);
+console.log(returnval)
+//console.log(name + "is a good boy");
+//console.log(name1 + "is a good boy");
+//console.log(name2 + "is a good boy");
+//console.log(name3+ "is a good boy");
